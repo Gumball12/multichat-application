01620cb8812837ec5d7909fc5c679ce04df39490
HaeJun Seo <kan02134@gmail.com>
2019-05-30 22:48:46 +0900

    Feat: Send data

--- Client.java
+++ Client.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
-  public static void main (String... args) {
-    System.out.println("Client");
+  public static void main (String... args) throws IOException, InterruptedException {
+    String s = "client 2";
+    byte[] buf = (s + " ").getBytes();
+
+    DatagramSocket sock = new DatagramSocket();
+    InetAddress addr = InetAddress.getByName("127.0.0.1");
+
+    DatagramPacket p = new DatagramPacket(buf, buf.length, addr, 10101);
+
+    for (int i = 0; i < 100; i++) {
+      Thread.sleep(1000);
+      sock.send(p);
+    }
+    sock.close();
   }
 }
