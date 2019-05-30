81b67f156621009d92089e21c7b790a8359e81ad
HaeJun Seo <kan02134@gmail.com>
2019-05-30 22:48:20 +0900

    Feat: Implement serve data

--- Server.java
+++ Server.java
@@ -1,5 +1,23 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
-  public static void main (String... args) {
-    System.out.println("Server");
+  public static void main (String... args) throws IOException {
+    // init socket, packet
+    DatagramSocket sock = new DatagramSocket(10101);
+    byte[] buf = new byte[256];
+    DatagramPacket p = new DatagramPacket(buf, buf.length);
+
+    // init user lists
+
+    // serve loop
+    for (int i = 0; i < 100; i++) {
+      sock.receive(p);
+      System.out.println(new String(buf));
+    }
+
+    // close socket
+    sock.close();
   }
 }
