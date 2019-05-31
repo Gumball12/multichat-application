447973abd7e26170f09103a853f6d9d4f91f8f9e
HaeJun Seo <kan02134@gmail.com>
2019-05-31 15:29:00 +0900

    Feat: init msg, user in/out notify

--- Server.java
+++ Server.java
@@ -1,3 +1,11 @@
+/**
+ * java multi-chat application (Server-side)
+ * 
+ * @author HeaJun Seo
+ * @since 2019-05-31
+ * @repo https://github.com/Gumball12/multichat-application
+ */
+
 // import modules
 import java.io.IOException;
 
@@ -27,15 +35,16 @@ public class Server {
     sendorSock = new DatagramSocket();
     addr = InetAddress.getByName("127.0.0.1");
 
-    // System.out.println("\\port".matches("^\\\\port"));
-
     // define socket (try-with resource statement)
-    try (DatagramSocket sock = new DatagramSocket(10100)) { // server: 10100)      
+    try (DatagramSocket sock = new DatagramSocket(10100)) { // server: 10100      
       // declare buffer, packet
       byte[] buf = new byte[256];
       DatagramPacket pack = new DatagramPacket(buf, buf.length);
 
       String message = null;
+
+      // state message
+      System.out.println("Server is ready!");
 
       // serve loop
       while (true) {
@@ -55,6 +64,12 @@ public class Server {
           // add user
           String[] userdata = message.substring(7).split("\\|"); // get port(0) and username(1) using regular expression
           users.put(userdata[0], userdata[1]); // set user data
+          sendor(userdata[1] + " 입장");
+        } else if (message.matches("^//close:.*")) {
+          // remove user
+          String[] userdata = message.substring(9).split("\\|"); // get userdata (port: [0], username: [1])
+          users.remove(userdata[0]); // using port number
+          sendor(userdata[1] + " 퇴장");
         } else {
           // send message to users
           sendor(message);
